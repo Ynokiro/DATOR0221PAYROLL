6b0a175910e05534aa35a5688911ce1bf1147ec6
jobelaraw <joshuaangelodator2006@gmail.com>
2026-02-21 18:07:35 +0800

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DATOR0221PAYROLL
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
