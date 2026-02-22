ebe950ba3b422574055f03bc7fcd55830e9e1170
Ynokiro <joshuaangelodator2006@gmail.com>
2026-02-22 11:59:37 +0800

    Implement Payroll class with user input for payroll
    Added a Payroll class with a Main method for payroll processing.

--- Program.cs
+++ Program.cs
@@ -1,10 +1,39 @@
-﻿namespace DATOR0221PAYROLL
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
+﻿using System;
+class Payroll {
+
+    static void Main(string[] args) { 
+    
+        string employeeName;
+        double hoursWorked, BasicPay, Overtime_worked, time_in, time_out, Employee_ID;
+
+        Console.WriteLine("-----Payroll System-----");
+
+        Console.Write("Enter Employee Name: "); 
+        employeeName = Console.ReadLine();
+
+        Console.Write("Enter Employee ID: ");
+        Employee_ID = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Enter Time In (12-hour format | EX. 9.00 for 9 AM): ");
+        time_in = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Enter Time Out (12-hour format | EX. 5.00 for 5 PM): ");  
+        time_out = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Enter Basic Pay per MONTH: ");
+        BasicPay = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Enter Overtime worked per Hour: ");
+        Overtime_worked = Convert.ToDouble(Console.ReadLine()); 
+
+        Console.Write(" enter Hours Worked: ");
+        hoursWorked = Convert.ToDouble(Console.ReadLine());
+
+        
+
+
+
+
+
     }
 }
